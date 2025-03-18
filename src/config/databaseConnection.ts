a87ea5d5d49591155b5f8d145155e901193c58ff
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-18 11:15:11 +0530

    logg commit

--- src/config/databaseConnection.ts
+++ src/config/databaseConnection.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from "./data-source";
 
 export const databaseConnect = async () => {
+    console.log('enjhcg: ', process.env.DATABASE_URL)
     try {
         await AppDataSource.initialize();
         console.log("database connected successfully");
