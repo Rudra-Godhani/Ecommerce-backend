bd7af3a4ef4db1b0dd57ae2f5cfad0216deb33b6
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-21 11:29:02 +0530

    added searchedProducts route

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { filteredProducts, getAllProducts, sortedProducts } from "../controller/ProductController";
+import { filteredProducts, getAllProducts, searchedProducts, sortedProducts } from "../controller/ProductController";
 
 const router = express.Router();
 
 router.get("/getallproducts",getAllProducts);
 router.get("/filteredproducts",filteredProducts);
 router.get("/sortedproducts",sortedProducts);
+router.get("/searchedproducts",searchedProducts);
 
 export default router;
