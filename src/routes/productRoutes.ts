5c64dfd0f4582b357b03b279ca0f4030d7ec88f9
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-27 17:59:01 +0530

    removed cart routes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import { addProductToCart, filteredProducts, getAllProducts, getCart, searchedProducts } from "../controller/ProductController";
+import { filteredProducts, getAllProducts, searchedProducts } from "../controller/ProductController";
 
 const router = express.Router();
 
 router.get("/getallproducts",getAllProducts);
 router.get("/filteredproducts",filteredProducts);
 router.get("/searchedproducts",searchedProducts);
-router.post("/addproducttocart",addProductToCart);
-router.get("/getcart", getCart);
 
 export default router;
