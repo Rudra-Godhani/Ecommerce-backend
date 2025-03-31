fa5c59bd1fb61353eac0cc4e630391f45bde109f
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-31 17:50:28 +0530

    updated code according to Category and Brand Entity

--- src/insertProducts.ts
+++ src/insertProducts.ts
@@ -1,38 +1,62 @@
 import { AppDataSource } from "./config/databaseConnection";
 import { productsData } from "./data/allProductsData";
-import { Product } from "./models/Product";
+import { Brand, Category, Product } from "./models/Product";
 
 const productRepository = AppDataSource.getRepository(Product);
+const categoryRepository = AppDataSource.getRepository(Category);
+const brandRepository = AppDataSource.getRepository(Brand);
+
 export const insertData = async () => {
     try {
-         const existingProductsCount = await productRepository.count();
-         console.log(existingProductsCount);
-         if (existingProductsCount > 0) {
-             console.log("Products already exist. Skipping insertion.");
-             return;
-         }
+        const existingProductsCount = await productRepository.count();
+        console.log(existingProductsCount);
+        if (existingProductsCount > 0) {
+            console.log("Products already exist. Skipping insertion.");
+            return;
+        }
 
-        const products = productsData.map((item) => {
-            const product = new Product();
-            product.title = item.title;
-            product.descriptionSmall = item.descriptionSmall;
-            product.descriptionLong = item.descriptionLong;
-            product.price = item.price;
-            product.retailPrice = item.retailPrice;
-            product.images = item.images;
-            product.colors = item.colors;
-            product.availability = item.availability;
-            product.reviewsText = item.reviewsText;
-            product.noOfReviews = item.noOfReviews;
-            product.rating = item.rating;
-            product.brand = item.brand;
-            product.category = item.category;
-            product.additionalInformation = item.additionalInformation;
-            return product;
-        });
+        for (const productData of productsData) {
+            let category = await categoryRepository.findOne({
+                where: { name: productData.category },
+            });
+            if (!category) {
+                category = categoryRepository.create({
+                    name: productData.category,
+                });
+                await categoryRepository.save(category);
+            }
 
-        await productRepository.save(products);
-        console.log(`${products.length} products inserted successfully`);
+            let brand = await brandRepository.findOne({
+                where: { name: productData.brand },
+            });
+            if (!brand) {
+                brand = brandRepository.create({
+                    name: productData.brand,
+                    category: category,
+                });
+                await brandRepository.save(brand);
+            }
+
+            const product = productRepository.create({
+                title: productData.title,
+                descriptionSmall: productData.descriptionSmall,
+                descriptionLong: productData.descriptionLong,
+                price: productData.price,
+                retailPrice: productData.retailPrice,
+                images: productData.images,
+                colors: productData.colors,
+                availability: productData.availability,
+                reviewsText: productData.reviewsText,
+                noOfReviews: productData.noOfReviews,
+                rating: productData.rating,
+                category: category,
+                brand: brand,
+                additionalInformation: productData.additionalInformation,
+            });
+            await productRepository.save(product);
+        }
+
+        console.log("products added successfullly");
     } catch (error) {
         console.log("Erorr inserting products:", error);
     }
