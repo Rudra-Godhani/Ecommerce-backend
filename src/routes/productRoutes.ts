c6703e5eaf29cff904b2a29b2812a6c902535eb4
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-31 17:55:47 +0530

    created routes for getProduct,getAllCategories,getAllCategories

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { filteredProducts, getAllProducts, searchedProducts } from "../controller/ProductController";
+import {
+    filteredProducts,
+    getAllBrands,
+    getAllCategories,
+    getAllProducts,
+    getProduct,
+    searchedProducts,
+} from "../controller/ProductController";
 
 const router = express.Router();
 
-router.get("/getallproducts",getAllProducts);
-router.get("/filteredproducts",filteredProducts);
-router.get("/searchedproducts",searchedProducts);
+router.get("/getallproducts", getAllProducts);
+router.get("/filteredproducts", filteredProducts);
+router.get("/searchedproducts", searchedProducts);
+router.get("/getproduct/:productid", getProduct);
+router.get("/getallcategories", getAllCategories);
+router.get("/getallbrands", getAllBrands);
 
 export default router;
