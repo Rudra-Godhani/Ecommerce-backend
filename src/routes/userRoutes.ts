2f2b4871969a02a68d7dc42729bf690a39dc73d9
Rudra-Godhani <rudragodhani12@gmail.com>
2025-04-07 17:33:25 +0530

    added address routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,17 @@
 import express from "express";
-import { getUser, getAllUsers, login, logout, register, updateProfile, updatePassword } from "../controller/userController";
+import {
+    getUser,
+    getAllUsers,
+    login,
+    logout,
+    register,
+    updateProfile,
+    updatePassword,
+    addNewAddress,
+    updateAddress,
+    getUserAddresses,
+    deleteAddress,
+} from "../controller/userController";
 import { isAuthenticated } from "../middleware/auth";
 import { forgotPassword, resetPassword } from "../controller/resetPassword";
 
@@ -7,11 +19,15 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout",isAuthenticated, logout);
+router.get("/logout", isAuthenticated, logout);
 router.get("/getalluser", getAllUsers);
-router.get("/getuser",isAuthenticated, getUser);
-router.put("/update/profile",isAuthenticated, updateProfile);
-router.put("/update/password",isAuthenticated, updatePassword);
+router.get("/getuser", isAuthenticated, getUser);
+router.post("/addnewaddress", isAuthenticated, addNewAddress);
+router.put("/update/address/:addressId", isAuthenticated, updateAddress);
+router.get("/getuseraddresses", isAuthenticated, getUserAddresses);
+router.delete("/delete/address/:addressId", isAuthenticated, deleteAddress);
+router.put("/update/profile", isAuthenticated, updateProfile);
+router.put("/update/password", isAuthenticated, updatePassword);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
