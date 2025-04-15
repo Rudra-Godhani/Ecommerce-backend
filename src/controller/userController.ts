58aa4064c8fd7065fa0ad7c26f2bcdb663fbcd11
Rudra-Godhani <rudragodhani12@gmail.com>
2025-04-15 14:19:02 +0530

    updated cookie option

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -118,10 +118,11 @@ export const login = catchAsyncErrorHandler(
             const { password: _, ...userWithoutPassword } = user;
 
             const options = {
-                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
                 httpOnly: true,
                 secure: true,
-                sameSite: "strict" as const
+                sameSite: "strict" as const,
+                path: "/",
+                maxAge: 7 * 24 * 60 * 60 * 1000,
             };
 
             res.cookie("token", token, options).status(200).json({
