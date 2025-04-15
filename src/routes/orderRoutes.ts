80fa46172e277471ed81f2dd8a877afe12d6df20
Rudra-Godhani <rudragodhani12@gmail.com>
2025-04-15 12:22:23 +0530

    changed tempOrder file name to orderController

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth";
-import { getMyOrderById, getMyOrders } from "../controller/OrderController";
+import { getMyOrderById, getMyOrders } from "../controller/orderController"
 
 const router = express.Router();
 
