65f67b8ed59e320dda608d6a55117624b072b5ff
Rudra-Godhani <rudragodhani12@gmail.com>
2025-04-15 12:27:48 +0530

    added logs

--- src/config/databaseConnection.ts
+++ src/config/databaseConnection.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+console.log("DATABASE_URL 1: ", process.env.DATABASE_URL);
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
@@ -14,11 +15,16 @@ export const AppDataSource = new DataSource({
     },
 });
 
+console.log("DATABASE_URL 2: ", process.env.DATABASE_URL);
 export const databaseConnect = async () => {
+    console.log("DATABASE_URL 3: ", process.env.DATABASE_URL);
     try {
+        console.log("DATABASE_URL 4: ", process.env.DATABASE_URL);
         await AppDataSource.initialize();
+        console.log("DATABASE_URL 5: ", process.env.DATABASE_URL);
         console.log("database connected successfully");
     } catch (error) {
+        console.log("DATABASE_URL 6: ", process.env.DATABASE_URL);
         console.log("Database connection error:", error);
     }
 };
